5c5016cf3ddfe2e9be3639c47094b3d76885d4c5
Gabriel Muñoz <gabriel.munoz@aytos.es>
2018-02-08 18:26:37 +0100

    Nuevos archivos necesarios para diversas imágenes

---
+++ java8/v1-windows/descargajava.ps1
@@ -0,0 +1,6 @@
+$source = "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-windows-x64.exe"
+$destination = "C:\jdk8u121.exe"
+$client = new-object System.Net.WebClient 
+$cookie = "oraclelicense=accept-securebackup-cookie"
+$client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie) 
+$client.downloadFile($source, $destination)
---
+++ libreoffice/v1-windows/libreoffice.bat
@@ -0,0 +1,1 @@
+C:\LibreOffice\program\soffice.exe --headless --nofirststartwizard --accept="socket,host=0.0.0.0,port=8101;urp;"
