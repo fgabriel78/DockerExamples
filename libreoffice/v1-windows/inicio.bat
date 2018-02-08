d5da08190128a30d4f5819b0da164488d861d60b
Gabriel Muñoz <gabriel.munoz@aytos.es>
2018-02-08 18:24:47 +0100

    Eliminados archivos que ya no son necesarios

--- libreoffice/v1-windows/inicio.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\LibreOffice\program\soffice --nologo --norestore --invisible --headless --accept='socket,host=0,port=8101,tcpNoDelay=1;urp;'
-ping -t localhost
--- libreoffice/v1-windows/servicio.bat
+++
@@ -1,2 +0,0 @@
-cd c:\LibreOffice
-.\nssm.exe install LibreOffice C:\LibreOffice\program\soffice --nologo --norestore --invisible --headless --accept='socket,host=0,port=8101,tcpNoDelay=1;urp;'
